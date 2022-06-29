288429968cb48a0feae205fc91f8dd2c7e4fad5d
Brian Hache <brnhache@gmail.com>
2022-06-28 20:31:12 -0400

    Getting repo data from local file. Refactored the array to make it flat with the spread operator instead of using .push().

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,15 +1,22 @@
 import { Router, Request, Response } from 'express';
 import axios from 'axios';
+import fs from 'fs';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
   res.status(200);
-
+  
+  let data: any[] = [];
+  
+  //Grab repo data
   try{
-    let data = await (await axios.get('https://api.github.com/users/silverorange/repos')).data;
+    data = [
+      ...data, 
+      JSON.parse(fs.readFileSync('./data/repos.json').toString()), //Local File
+      await (await axios.get('https://api.github.com/users/silverorange/repos')).data //Directly from Github
+    ];   
     res.json(data);
   }catch(error){
     if(error instanceof Error){
