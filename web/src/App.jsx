31471f3fc8d43bf7d63e2807623a69fa9e685fdb
Brian Hache <brnhache@gmail.com>
2022-06-29 18:56:58 -0400

    Created a rough list, poached some styling from the react welcome page for the header. Added some styling to App.css for the list items.
    Added placeholders for missing data.
    
    Can't see why the fork count is not displaying yet.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,26 +1,49 @@
 import React, { useState, useEffect } from 'react';
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, ListGroup } from 'react-bootstrap';
 
 export function App() {
-  const [repos, setRepos ] = useState([]);
+  const [repos, setRepos] = useState([]);
   useEffect(() => {
     getData();
   }, []);
-  const getData = async() => {
+  const getData = async () => {
     const data = await (await fetch('/repos')).json();
-    const newRepos = data.map((repo) => {return {
-      name: repo.name,
-      description: repo.description,
-      language: repo.language,
-      forkCount: repo.forks_count
-    }})
+    const newRepos = data.map((repo) => {
+      console.log(repo.forks_count);
+      const repoName = repo.name || 'No name specified';
+      const repoDescription = repo.description || 'No description specified';
+      const repoLanguage = repo.language || 'No language specified';
+      const repoForksCount = repo.forks_count || 0;
+      return {
+        name: repoName,
+        description: repoDescription,
+        language: repoLanguage,
+        forkCount: repoForksCount.toString(),
+      };
+    });
     setRepos(newRepos);
-    console.log(newRepos)
-  }
+  };
   return (
     <div className="App">
-      
+      <h1 className="mb-4, App-header">Silverorange Coding Exercise</h1>
+      <Container>
+        <ListGroup>
+          {repos.map((repo, idx) => {
+            return (
+              <ListGroup.Item className="Repo-item" variant="dark" key={idx}>
+                <h3>Repo Name: {repo.name}</h3>
+                <h3>Repo Description: {repo.description}</h3>
+                <h3>Repo Language: {repo.language}</h3>
+                <h3>Repo Forks Count: {repo.forks_count}</h3>
+              </ListGroup.Item>
+            );
+          })}
+        </ListGroup>
+      </Container>
     </div>
   );
 }
