bb839f8ea16fb3ba048ac183e9e95f8017078ed3
Brian Hache <brnhache@gmail.com>
2022-06-28 20:58:21 -0400

    fixed the array properly this time and filtered the array so that we're only returning items with fork = true to the front end.

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -8,15 +8,12 @@ repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
   res.status(200);
   
-  let data: any[] = [];
-  
   //Grab repo data
   try{
-    data = [
-      ...data, 
-      JSON.parse(fs.readFileSync('./data/repos.json').toString()), //Local File
-      await (await axios.get('https://api.github.com/users/silverorange/repos')).data //Directly from Github
-    ];   
+    let data = [ 
+      ...JSON.parse(fs.readFileSync('./data/repos.json').toString()), //Local File
+      ...await (await axios.get('https://api.github.com/users/silverorange/repos')).data //Directly from Github
+    ].filter(item => item.fork === true);   
     res.json(data);
   }catch(error){
     if(error instanceof Error){
