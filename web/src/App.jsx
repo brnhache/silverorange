b49fede79625f6026155be920538f5bfd866fe0e
Brian Hache <brnhache@gmail.com>
2022-06-30 18:15:03 -0400

    Having trouble with typescript slowing me down - I thought that changing the filetype to .jsx would remove the typescript rules but apparently not. More good practice!
    Added a modal to display the repo details, just dummy data in the
    details for now. Hopefully the readme.md files aren't too big to make a
    modal a reasonable choice.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,32 +3,49 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Container, ListGroup, Toast, Navbar, Button } from 'react-bootstrap';
+import {
+  Container,
+  ListGroup,
+  Toast,
+  Navbar,
+  Button,
+  Modal,
+} from 'react-bootstrap';
 
 export function App() {
+  const [data, setData] = useState([]);
   const [repos, setRepos] = useState([]);
   const [renderedRepos, setRenderedRepos] = useState([]);
   const [showErrorToast, setShowErrorToast] = useState(false);
   const [toastError, setToastError] = useState('');
   const [languages, setLanguages] = useState([]);
+  const [showRepoDetails, setShowRepoDetails] = useState(false);
+  const [repoDetails, setRepoDetails] = useState({
+    latestCommitDate: 't',
+    latestCommitAuthor: 't',
+    latestCommitMessage: 't',
+    readMe: 't',
+  });
 
   useEffect(() => {
     const getData = async () => {
-      let data = await fetch('/repos');
-      if (data.ok) {
-        data = (await data.json()).sort((a, b) => {
+      let _data = await fetch('/repos');
+      if (_data.ok) {
+        _data = (await _data.json()).sort((a, b) => {
           const date_a = new Date(a.created_at).getTime();
           const date_b = new Date(b.created_at).getTime();
           return date_b - date_a;
         });
-        const newRepos = data.map((repo) => {
+        const newRepos = _data.map((repo) => {
           return {
+            id: repo.id,
             name: repo.name || 'No name specified',
             description: repo.description || 'No description specified',
             language: repo.language || 'No language specified',
             forkCount: repo.forks_count,
           };
         });
+        setData((d) => _data);
         setRepos(newRepos);
         setRenderedRepos(newRepos);
         //Collect all of the languages
@@ -40,7 +57,7 @@ export function App() {
         });
         setLanguages((l) => _languages);
       } else {
-        openShowErrorToast(data);
+        openShowErrorToast(_data);
       }
     };
     getData();
@@ -52,11 +69,26 @@ export function App() {
       const filteredRepos = repos.filter((repo) => repo.language === lang);
       setRenderedRepos(filteredRepos);
     }
+    console.log(data);
   };
 
   const openShowErrorToast = async (error) => {
     setToastError(await error.json());
     setShowErrorToast(true);
+  };
+
+  const handleOpenRepoDetails = async (repo_id) => {
+    if (showRepoDetails) {
+      return;
+    }
+    // const selectedRepo = data.find((repo) => (repo.id = repo_id));
+    setRepoDetails({
+      latestCommitDate: 'date',
+      latestCommitAuthor: 'author',
+      latestCommitMessage: 'message',
+      readMe: '<h1>Helloooooo Readme</h1>',
+    });
+    setShowRepoDetails(true);
   };
 
   return (
@@ -93,7 +125,12 @@ export function App() {
         <ListGroup>
           {renderedRepos.map((repo, idx) => {
             return (
-              <ListGroup.Item className="Repo-item" variant="dark" key={idx}>
+              <ListGroup.Item
+                onClick={() => handleOpenRepoDetails(repo.id)}
+                className="Repo-item"
+                variant="dark"
+                key={idx}
+              >
                 <h3>Repo Name: {repo.name}</h3>
                 <h3>Repo Description: {repo.description}</h3>
                 <h3>Repo Language: {repo.language}</h3>
@@ -103,6 +140,22 @@ export function App() {
           })}
         </ListGroup>
       </Container>
+      <Modal show={showRepoDetails} onHide={() => setShowRepoDetails(false)}>
+        <Modal.Header>
+          <Modal.Title>Repo Deatils</Modal.Title>
+        </Modal.Header>
+        {repoDetails.readMe.length > 0 && (
+          <Modal.Body>{repoDetails.readMe}</Modal.Body>
+        )}
+        <Modal.Footer>
+            <Button
+              variant="secondary"
+              onClick={() => setShowRepoDetails(false)}
+            >
+              Close
+            </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
